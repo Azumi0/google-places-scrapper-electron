3bee724315a4e45180c0110be930c91a9229e502
Przemyslaw Wrzeszcz <przemyslaw.wrzeszcz@gmail.com>
2018-09-17 22:18:09 +0200

    working export, missing icons, rating is not saved dunno why tho

--- src/index.js
+++ src/index.js
@@ -1,4 +1,92 @@
 import { app, BrowserWindow, ipcMain } from 'electron';
+import locutusEmpty from "locutus/php/var/empty";
+import xl from 'excel4node';
+
+const googleMapsClient = require('@google/maps').createClient({
+  key: '***REMOVED***',
+  Promise: Promise,
+});
+
+const googleMapsPlacesFiledsMapping = {
+  name: 'nazwa',
+  formatted_address: 'adres fizyczny',
+  locality: 'miasto',
+  postal_code: 'kod pocztowy',
+  formatted_phone_number: 'numer telefonu',
+  website: 'adres strony www',
+  url: 'link do wizytówki',
+  rating: 'ocena miejsca',
+};
+
+const parseAddressComponents = (data) => {
+  const final = {};
+
+  if (locutusEmpty(data)) {
+    return final;
+  }
+
+  data.map((el) => {
+    final[el.types[0]] = el.long_name;
+  });
+
+  return final;
+};
+
+const TYPES = {
+  undefined: 'undefined',
+  number: 'number',
+  boolean: 'boolean',
+  string: 'string',
+  '[object Function]': 'function',
+  '[object RegExp]': 'regexp',
+  '[object Array]': 'array',
+  '[object Date]': 'date',
+  '[object Error]': 'error',
+};
+const valueType = o => (TYPES[typeof o] || TYPES[Object.prototype.toString.call(o)] || (o ? 'object' : 'null'));
+
+const getSafeErrorValue = (element) => {
+  const type = valueType(element);
+
+  if (type === 'string') {
+    return element;
+  }
+
+  if (type === 'array') {
+    return element.join(', ');
+  }
+
+  if (type === 'boolean') {
+    return (element ? 'true' : 'false');
+  }
+
+  if (type === 'date') {
+    return element.toString();
+  }
+
+  if (type === 'number' || type === 'null') {
+    return element;
+  }
+
+  if (element.toString) {
+    return element.toString();
+  }
+
+  if (element.message) {
+    return element.message;
+  }
+
+  if (type === 'object') {
+    return element;
+  }
+
+  if (type === 'undefined') {
+    return 'undefined';
+  }
+
+  return '';
+};
+
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -20,7 +108,7 @@ const createWindow = () => {
   mainWindow.loadURL(`file://${__dirname}/markup/index.html`);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', () => {
@@ -56,10 +144,82 @@ app.on('activate', () => {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
 
+// https://www.youtube.com/watch?v=kN1Czs0m1SU
+// https://github.com/bradtraversy/electronshoppinglist
+
 ipcMain.on('scrape:start', (e, data) => {
-  console.log(data);
-  // mainWindow.webContents.send('item:add', item);
-  // addWindow.close();
-  // Still have a reference to addWindow in memory. Need to reclaim memory (Grabage collection)
-  //addWindow = null;
+  googleMapsClient.geocode({
+    address: data.city,
+  }).asPromise()
+    .then((response) => {
+      const geocode = response.json.results[0].geometry.location;
+
+      googleMapsClient.places({
+        query: data.keywords,
+        language: 'pl',
+        location: [geocode.lat, geocode.lng],
+        radius: 50000,
+      })
+        .asPromise()
+        .then((responsePlaces) => {
+          const fetchPromises = [];
+
+          responsePlaces.json.results.map((el) => {
+            fetchPromises.push(googleMapsClient.place({
+              placeid: el.place_id,
+              language: 'pl',
+            }).asPromise());
+          });
+
+          Promise.all(fetchPromises).then((fethedData) => {
+            const wb = new xl.Workbook();
+            const ws = wb.addWorksheet('Sheet 1');
+            let rowIndex = 1;
+
+            Object.entries(googleMapsPlacesFiledsMapping).map((mapEntry, mapIndex) => {
+              ws.cell(rowIndex, (mapIndex + 1)).string(mapEntry[1]);
+            });
+
+            rowIndex += 1;
+
+            fethedData.map((fetchedRecord) => {
+              if (parseInt(fetchedRecord.status) === 200) {
+                const parsedRecord = Object.assign(parseAddressComponents(fetchedRecord.json.result.address_components), fetchedRecord.json.result);
+
+                Object.entries(googleMapsPlacesFiledsMapping)
+                  .map((mapEntry, mapIndex) => {
+                    ws.cell(rowIndex, (mapIndex + 1))
+                      .string((locutusEmpty(parsedRecord[mapEntry[0]])) ? '' : parsedRecord[mapEntry[0]]);
+                  });
+
+                rowIndex += 1;
+              }
+            });
+
+            wb.write(data.savePath);
+
+            mainWindow.webContents.send('scrape:finish', {
+              success: true,
+              message: '',
+            });
+          }).catch((errDetails) => {
+            mainWindow.webContents.send('scrape:finish', {
+              success: false,
+              message: getSafeErrorValue(errDetails),
+            });
+          });
+        })
+        .catch((errPlaces) => {
+          mainWindow.webContents.send('scrape:finish', {
+            success: false,
+            message: getSafeErrorValue(errPlaces),
+          });
+        });
+    })
+    .catch((err) => {
+      mainWindow.webContents.send('scrape:finish', {
+        success: false,
+        message: getSafeErrorValue(err),
+      });
+    });
 });
--- src/markup/js/main.js
+++ src/markup/js/main.js
@@ -1,7 +1,11 @@
 const electron = require('electron');
 const jQuery = require('jquery');
+// const remote = require('remote');
+const path = require('path');
+const fs = require('fs');
 
-const { ipcRenderer } = electron;
+const { ipcRenderer, remote } = electron;
+const dialog = remote.dialog;
 
 jQuery(window).on('load', () => {
   const $ = jQuery;
@@ -9,14 +13,48 @@ jQuery(window).on('load', () => {
   $('#exportForm').on('submit', (e) => {
     e.preventDefault();
 
-    $('#mainPreloader').show();
+    dialog.showSaveDialog({
+      filters: [
+        {
+          name: 'Arkusz xls MS Excel',
+          extensions: ['xls'],
+        },
+      ],
+    }, (savePath) => {
+      if (savePath === undefined) return;
 
-    const keywords = $('#keywords').val();
-    const city = $('#city').val();
+      const saveDirname = path.dirname(savePath);
+      fs.access(saveDirname, fs.constants.W_OK, (err) => {
+        if (err) {
+          dialog.showErrorBox('Błąd zapisu pliku', 'Nie można zapisać pliku w wybranym miejscu - brak dostępu.');
 
-    ipcRenderer.send('scrape:start', {
-      keywords,
-      city,
+          return;
+        }
+
+        $('#mainPreloader').show();
+
+        const keywords = $('#keywords').val();
+        const city = $('#city').val();
+
+        ipcRenderer.send('scrape:start', {
+          savePath,
+          keywords,
+          city,
+        });
+      });
     });
+  });
+
+  ipcRenderer.on('scrape:finish', (e, result) => {
+    $('#mainPreloader').hide();
+
+    if (result.success) {
+      dialog.showMessageBox({
+        message: 'Eksport wizytówek zakończony powodzeniem!',
+        buttons: ['OK'],
+      });
+    } else {
+      dialog.showErrorBox('Błąd exportu', `Wystąpił błąd podczas eksportu.\nOtrzymaliśmy następujący błąd: ${result.message}`);
+    }
   });
 });
